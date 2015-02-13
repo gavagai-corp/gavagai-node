f75999373a28e785bf117e5b601d17a94341e8c8
Johan Dewe <johan.dewe@gavagai.se>
2015-02-13 12:59:47 +0100

    Add options to stories resource, language defaults to 'en'

--- spec/client.stories.spec.js
+++ spec/client.stories.spec.js
@@ -1,40 +1,74 @@
 'use strict';
 
-var should = require('chai').should();
+var should = require('should');
 var nock = require('nock');
 var Gavagai = require('../lib');
-var Stories = require('../lib/Stories');
 
-describe('The gavagai API stories resource #find', function () {
-    var stories = new Stories(new Gavagai('abc123'));
+describe('The gavagai API stories resource', function () {
+    var docs = require('./data/documents.json');
+    var gavagai = new Gavagai('abc123');
     var api;
 
+    it('should have default language', function(done) {
+        validateApiRequest(function(body) {
+            var defaultLanguage = 'en';
+            body.language.should.equal(defaultLanguage);
+            return requiredValues(body);
+        });
+        gavagai.stories(docs, function (err, data) {
+            api.isDone().should.equal(true, "Matching API call.");
+            done();
+        });
+    });
+
     it('should accept an array of document objects', function (done) {
-        var docs = require('./data/documents.json');
-        stories.find(docs, function (err, data) {
-            api.isDone().should.equal(true);
+        validateApiRequest(requiredValues);
+        gavagai.stories(docs, function (err, data) {
+            api.isDone().should.equal(true, "Matching API call.");
             done();
         });
     });
 
     it.skip('should accept an array of texts', function (done) {
-        stories.find(['this is a text', 'this is text 2', 'this is a third text'], function () {
-            api.isDone().should.equal(true);
+        validateApiRequest(requiredValues);
+        gavagai.stories(['this is a text', 'this is text 2', 'this is a third text'], function () {
+            api.isDone().should.equal(true, "Matching API call.");
             done();
         })
     });
 
-    beforeEach(function () {
-        api = nock('https://api.gavagai.se:443')
-            .post('/v3/stories?language=en&apiKey=abc123', validate)
-            .reply(200, 'OK');
+    it('should handle custom options', function (done) {
+        var options = {
+            language: 'sv',
+            ignore: ['ignoreword', 'ignore phrase'],
+            terms: ['term', 'term phrase']
+        };
+
+        validateApiRequest(function(body) {
+            body.language.should.equal('sv');
+            body.ignore.should.eql(['ignoreword', 'ignore phrase'], 'parameter "ignore"');
+            body.terms.should.eql(['term', 'term phrase'], 'parameter "terms"');
+            return requiredValues(body);
+        });
+
+        gavagai.stories(docs, options, function () {
+            api.isDone().should.equal(true, "Matching API call.");
+            done();
+        });
     });
 
-    function validate(body) {
-        body.should.include.keys('documents');
-        body.documents.should.be.an('Array');
-        body.documents[0].should.be.an('object');
-        body.documents[0].should.include.keys('title', 'body');
+    function validateApiRequest(validator) {
+        api = nock('https://api.gavagai.se:443')
+            .filteringPath(/language=[^&]*/g, 'language=XX')
+            // TODO: remove detailed and language from url
+            .post('/v3/stories?detailed=true&language=XX&apiKey=abc123', validator)
+            .reply(200, 'OK');
+    }
+
+    function requiredValues(body) {
+        body.should.have.property('documents');
+        body.documents.should.be.an.Array;
+        body.documents[0].should.have.properties('title', 'body');
         return true;
     }
 
