d6532081d99e10df361abecc97e6a0828d266bd9
Johan Dewe <johan.dewe@gavagai.se>
2015-02-18 10:33:37 +0100

    Add stories example

---
+++ examples/stories.js
@@ -0,0 +1,15 @@
+var gavagai = require('../lib');
+var client = gavagai('GAVAGAI_APIKEY');
+
+var array = [
+    'Stayed here for 3 nights at the beginning of a trip of California. Could not say enough good things about the hotel Monaco. Amazing staff, amazing rooms and the location is brilliant! First stay at a Kimpton hotel, but definitely not the last!!!',
+    'I did a lot of research looking for a hotel suite for our family vacation in San Francisco. The Hotel Monaco was a perfect choice. What friendly and delightful staff. I will miss the Grand Cafe, but I will make sure to come back to see their new offerings.',
+    'My partner and I spent four nights here over New Years and loved it. Super staff; lovely, quiet room; excellent location within easy walking to much of Downtown and an overall experience that was perfect.'
+];
+
+client.stories(array, function(err, data) {
+    if (err) {
+        console.error('error:', err);
+    }
+    console.log('stories returned:', data);
+});
